a61de95e63e190edeae50b3643291be547d8e2b6
Kaushal Modi <kaushal.modi@gmail.com>
2017-05-24 12:30:28 -0400

    Make .dir-locals.el not spam users; Improve hl-line experience
    - Instead of putting the autoloading code inside .dir-locals.el, it is
    much simpler to just ask the users interested in developing eless to
    simply eval the build/build.el.
    - Now if someone is simply opening eless.org to review it, they won't
    be spammed by the prompt to save the variables as safe.
    
    - The hl-line mode is now enabled globally, so that whatever buffers
    you open in the eless session, all will have that mode enabled.
    
    - Switched back to tango-dark; manoj-dark theme did not look good in
    org mode. The only reason I had switched away from tango-dark earlier
    was the bright-yellow default hl-line face. I just changed that face
    in this commit to be dark grey.
    
    - Update README; update credits.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,4 @@
 (nil
  . ((nil . ((indent-tabs-mode . nil)
-            (fill-column . 70)
-            (eval . (progn
-                      (when (buffer-file-name)
-                        (require 'vc-git) ;For `vc-git-root'
-                        (autoload
-                          'eless-build (expand-file-name "build/build.el" (vc-git-root (buffer-file-name)))
-                          "Use eless.org to generate the `eless' script and documentation."
-                          :interactive)
-                        (autoload
-                          'eless-install-dependencies (expand-file-name "build/build.el" (vc-git-root (buffer-file-name)))
-                          "Install dependencies for building `eless', in `eless-package-user-dir'."
-                          :interactive))))))
+            (fill-column . 70)))
     (org-mode . ((mode . auto-fill)))))
